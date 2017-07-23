8a26d20d1ae38d1e344f6eca2a7721ecc71b3757
YZJ <317493138@qq.com>
2017-07-23 22:57:30 +0800

    aa

--- fortune500.py
+++ fortune500.py
@@ -21,17 +21,18 @@ def getHTMLText(url):
         return ""
 
 
-def insert(s):
+def insert(s,year):
     conn = pymysql.connect(host='127.0.0.1',port = 3306,user='root',passwd='root',db='test',charset='utf8')
     cursor = conn.cursor()
     for tu in s:
-        print(tu[3])
+        #print(tu[3])
         db3 = tranferStr(tu[3])
         
-        print(db3)
+        #print(db3)
         db4 = tranferStr(tu[4])
-        sql = 'insert into fortune500 values({},\'{}\',\'{}\',{},{},\'{}\')'.format(tu[0],tu[1],tu[2],db3,db4,tu[5])
-        print(sql)
+        sql = 'insert into fortune500 values({},\'{}\',\'{}\',{},{},\'{}\',\'{}\')'.format(tu[0],tu[1],tu[2],db3,db4,tu[5],year)
+        if year == 2015:
+            print(sql)
         try:
             cursor.execute(sql)   
         except:
@@ -46,12 +47,21 @@ def tranferStr(str):
     return number
     
 def main():
-    href = 'http://www.fortunechina.com/fortune500/c/2017-07/20/content_286785.htm'
+    tubHref =['http://www.fortunechina.com/fortune500/c/2017-07/20/content_286785.htm',
+              'http://www.fortunechina.com/fortune500/c/2016-07/20/content_266955.htm',
+              'http://www.fortunechina.com/fortune500/c/2015-07/22/content_244435.htm']
     
-    html = getHTMLText(href)
-
-    s =re.findall('<tr>.*?<td>(.*?)</td>.*?<td>(.*?)</td>.*?_blank">(.*?)</a>.*?<td>(.*?)</td>.*?<td>(.*?)</td>.*?<td>(.*?)</td>',html)
-
-    insert(s)
+    #href = 'http://www.fortunechina.com/fortune500/c/2017-07/20/content_286785.htm'
+    
+    for href in tubHref:
+        
+        html = getHTMLText(href)
+        year = re.findall('c/(.*?)-',href)
+        year = year[0]
+        print(year)
+        s =re.findall('<tr>.*?<td>(.*?)</td>.*?<td>(.*?)</td>.*?_blank">(.*?)</a>.*?<td>(.*?)</td>.*?<td>(.*?)</td>.*?<td>(.*?)</td>',html)
+        if year == '2015':
+            print(s)
+        insert(s,year)
 
 main()
