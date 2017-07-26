9bc682cf8af3643c4e77e913eccb724a8a593c98
YZJ <317493138@qq.com>
2017-07-26 23:41:03 +0800

    socket

---
+++ socket/tsTclnt.py
@@ -0,0 +1,21 @@
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSock.send(data.encode())
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+    
+tcpCliSock.close()
---
+++ socket/tsTserv.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 22:45:25 2017
+
+@author: Yangzj
+"""
+
+from socket import * 
+from time import ctime
+
+HOST = ''
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM, 0)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+while True:
+    print('waiting for connection...')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('...connected from:', addr)
+    
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        tcpCliSock.send(b'[%s] %s' % ( bytes(ctime(), 'utf-8'), data))
+        
+    tcpCliSock.close()
+tcpSerSock.close()    
